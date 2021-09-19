8fe5d78ad2ef4d66cff2e3322b07cfa062c8d654
alsco77 <alsco77@pm.me>
2021-09-19 19:29:38 +0100

    chore: Refresh repo

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 <br/>
-<img src="https://i.ibb.co/5KpcCWy/photo-2021-09-07-09-47-44.jpg" width="420" >
 
-![CI](https://github.com/mathblocks/contracts/workflows/Test/badge.svg)
+## <br />
 
-<br />
+## Lagos
 
-MathBlocks
+---
--- test-utils/standardAccounts.ts
+++ test-utils/standardAccounts.ts
@@ -22,32 +22,6 @@ export class StandardAccounts {
 
     public dummy3: Account
 
-    public dummy4: Account
-
-    public dummy5: Account
-
-    public dummy6: Account
-
-    public dummy7: Account
-
-    public fundManager: Account
-
-    public fundManager2: Account
-
-    public questMaster: Account
-
-    public questSigner: Account
-
-    public mockSavingsManager: Account
-
-    public mockInterestValidator: Account
-
-    public mockRecollateraliser: Account
-
-    public mockMasset: Account
-
-    public mockRewardsDistributor: Account
-
     public async initAccounts(signers: Signer[]): Promise<StandardAccounts> {
         this.all = await Promise.all(
             signers.map(async (s) => ({
@@ -55,27 +29,7 @@ export class StandardAccounts {
                 address: await s.getAddress(),
             })),
         )
-        ;[
-            this.default,
-            this.governor,
-            this.other,
-            this.dummy1,
-            this.dummy2,
-            this.dummy3,
-            this.dummy4,
-            this.dummy5,
-            this.dummy6,
-            this.dummy7,
-            this.fundManager,
-            this.fundManager2,
-            this.questMaster,
-            this.questSigner,
-            this.mockSavingsManager,
-            this.mockInterestValidator,
-            this.mockRecollateraliser,
-            this.mockMasset,
-            this.mockRewardsDistributor,
-        ] = this.all
+        ;[this.default, this.governor, this.other, this.dummy1, this.dummy2, this.dummy3] = this.all
         return this
     }
 }
